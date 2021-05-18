80f3eab2dc15668e95223e5d1562fa3d71125812
tchintak <84365835+tchintak@users.noreply.github.com>
2021-05-18 09:40:52 +0530

    Add files via upload
    Initial commit_05_18_2021

---
+++ java/Activity1.java
@@ -0,0 +1,13 @@
+package activities;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		Car obj = new Car("Black","Manual",2014);
+obj.dispalychar();
+obj.accelarate();
+obj.brake();
+
+	}
+
+}
---
+++ java/Activity2.java
@@ -0,0 +1,11 @@
+package activities;
+
+public class Activity2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {10, 77, 10, 54, -11, 10};
+
+	}
+
+}
